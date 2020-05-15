7e3cc4e77b359d4f8695b92a604676f378ca8f2a
dog-verao <contatoaguiarti@gmail.com>
2020-05-15 16:05:40 +0100

    fix: typo on naming convention

--- src/CompoundComponent/List.js
+++ src/CompoundComponent/List.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import New from './New';
 import NewsProvider, { NewsContext } from './NewsContext';
 
-//Not the best naming convention, but I want to keep App.js with only a List component
+//Not the best naming structure, but I want to keep App.js with only a List component
 const StatefulList = () => {
     const news = useContext(NewsContext); 
     return <>{news && news.hits && news.hits.map((hit, i) => <StatefulList.New info={hit} key={i}/>)}</>
